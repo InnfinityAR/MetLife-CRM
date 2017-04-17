91ad2910ba7e487e6c96d4d9e94ec332470e685d
王雨 <523441705@qq.com>
2017-04-17 17:25:06 +0800

    init

---
+++ data/conf/db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'crm',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'crm_',
+);
